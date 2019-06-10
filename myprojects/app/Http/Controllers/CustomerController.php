b4da9e4dd628fac886ab2ec99097d32fbeb31f19
maz_leo <xtleon.xl@gmail.com>
2019-06-11 01:36:00 +0600

    get email from input filed
    and save it to databse

--- myprojects/app/Http/Controllers/CustomerController.php
+++ myprojects/app/Http/Controllers/CustomerController.php
@@ -23,14 +23,16 @@ class CustomerController extends Controller
         //get name from input-form
 
         $data = request()->validate([
-            'name' =>'required|min:3'  //we need minimum 3 character name
+            'name' =>'required|min:3',  //we need minimum 3 character name
+            'email' =>'required|email'  //we need need email and also a valid email
         ]);
 
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->save();
+        $customer = new Customer(); // create a object of customer database
+        $customer->name = request('name'); // add customer name to databse
+        $customer->email = request('email'); // add email to databse
+        $customer->save(); //save to database
 
-        return back();
+        return back(); //back to the previous function
        
     }
 }
