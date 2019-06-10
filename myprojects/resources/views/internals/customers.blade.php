95ca1591f891b58ed84378d4b36a6650349d882b
maz_leo <xtleon.xl@gmail.com>
2019-06-11 01:36:40 +0600

    add email input field and som validation

--- myprojects/resources/views/internals/customers.blade.php
+++ myprojects/resources/views/internals/customers.blade.php
@@ -9,12 +9,15 @@
     <h1>Customers Page</h1>
 
     <form action="customers" method="POST" class="pb-5">
-        <div class="input-group">
-            <input type="text" name="name">
+        <div class="input-group pb-2">
+            <input type="text" name="name" placeholder="enter your name "  value="{{ old('name') }} ">
+            <div> {{ $errors->first('name') }}</div>
+        </div>
+        <div class="input-group">  
+            <input type="email" name="email" placeholder="enter your email"value="{{ old('email') }} ">
+            <div> {{ $errors->first('email') }}</div>
         </div>
 
-        {{ $errors->first('name') }}
-        <br>
         <button type="submit">Add Customer</button>
 
         @csrf
@@ -24,7 +27,8 @@
 
     <ui>
         @foreach ($customers as $customer) <!-- blade syntax -->
-        <li>{{ $customer->name }}</li>
+        <li>{{ $customer->name }} <span class="text-muted">({{ $customer->email }})</span></li>
+       
         @endforeach
     </ui>
 @endsection
