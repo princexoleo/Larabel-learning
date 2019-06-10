c219c19762cefe22b5ab5d7db1a05dd5d7ec0ed4
maz_leo <xtleon.xl@gmail.com>
2019-06-11 02:23:45 +0600

    eloquent where clause

--- myprojects/app/Http/Controllers/CustomerController.php
+++ myprojects/app/Http/Controllers/CustomerController.php
@@ -10,12 +10,18 @@ class CustomerController extends Controller
     //
     public function list()
     {
-        $customers = Customer::all();
+        $activeCustomers = Customer::where('active',1)->get(); //get active customers form database by checking their active column
+        $inactiveCustomers = Customer::where('active',0)->get();
+
+       // $customers = Customer::all();
+
+       return view('internals.customers', compact('activeCustomers','inactiveCustomers')); //another option of passing data to view
        
 
-        return view('internals.customers',[
-            'customers'=>$customers,
-        ]);
+        // return view('internals.customers',[
+        //     'activeCustomers'=>$activeCustomer,//send Active customers details to view
+        //     'inactiveCustomers' => $inactiveCustomer, //send inActive customers details to view
+        // ]);
     }
 
     //
@@ -24,12 +30,14 @@ class CustomerController extends Controller
 
         $data = request()->validate([
             'name' =>'required|min:3',  //we need minimum 3 character name
-            'email' =>'required|email'  //we need need email and also a valid email
+            'email' =>'required|email', //we need need email and also a valid email
+            'active' =>'required'
         ]);
 
         $customer = new Customer(); // create a object of customer database
         $customer->name = request('name'); // add customer name to databse
         $customer->email = request('email'); // add email to databse
+        $customer->active = request('active'); // geta ctive status value from input form 
         $customer->save(); //save to database
 
         return back(); //back to the previous function
--- myprojects/resources/views/internals/customers.blade.php
+++ myprojects/resources/views/internals/customers.blade.php
@@ -26,6 +26,15 @@
                             <input type="email" name="email" placeholder="enter your email"value="{{ old('email') }} " class="form-control">
                             <div> {{ $errors->first('email') }}</div>
                         </div>
+
+                        <div class="form-group">
+                            <label for="active">Status</label>
+                            <select name="active" id="active" class="form-control">
+                                <option value="" disabled>Select customer status</option>
+                                <option value="1">Active</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
                 
                         <button type="submit" class="btn btn-primary">Add Customer</button>
                 
@@ -38,13 +47,22 @@
      <hr>
  
      <div class="row">
-         <div class="col-12">    
+         <div class="col-6">  
+             <h3>Active Customers</h3>  
             <ui>
-                @foreach ($customers as $customer) <!-- blade syntax -->
-                <li>{{ $customer->name }} <span class="text-muted">({{ $customer->email }})</span></li>
+                @foreach ($activeCustomers as $activeCustomer) <!-- active customer deatils -->
+                <li>{{ $activeCustomer->name }} <span class="text-muted">({{ $activeCustomer->email }})</span></li>
                 @endforeach
             </ui>
          </div>
+         <div class="col-6">    
+                <h3>Inactive Customers</h3>  
+                <ui>
+                    @foreach ($inactiveCustomers as $inactiveCustomer) <!-- inactive customers details syntax -->
+                    <li>{{ $inactiveCustomer->name }} <span class="text-muted">({{ $inactiveCustomer->email }})</span></li>
+                    @endforeach
+                </ui>
+             </div>
      </div>
 
 @endsection
